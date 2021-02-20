8bd3f08595ec8552f9eb22ff3bbda3688cb61777
liuzhe0523 <1272991460@qq.com>
2021-02-20 11:47:22 +0800

    首页

---
+++ LifestyleServices_android/app/src/main/java/cn/edu/hebtu/software/lifestyleservices_android/ExpressActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.hebtu.software.lifestyleservices_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ExpressActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_express);
+
+    }
+}
--- LifestyleServices_android/app/src/main/java/cn/edu/hebtu/software/lifestyleservices_android/MainActivity.java
+++ LifestyleServices_android/app/src/main/java/cn/edu/hebtu/software/lifestyleservices_android/MainActivity.java
@@ -1,7 +1,11 @@
 package cn.edu.hebtu.software.lifestyleservices_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button_takeout=findViewById(R.id.btn_takeout);
+        Button button_express=findViewById(R.id.btn_express);
+        button_takeout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,TakeoutActivity.class);
+                startActivity(intent);
+            }
+        });
+        button_express.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ExpressActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
